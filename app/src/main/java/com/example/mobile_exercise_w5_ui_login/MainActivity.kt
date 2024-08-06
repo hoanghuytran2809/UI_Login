2992252a32e738480d09c493aca89cb7332ec478
TranHuyHoang <hoanghuytran2809@gmail.com>
2024-08-07 02:45:32 +0700

    update login

--- app/src/main/java/com/example/mobile_exercise_w5_ui_login/MainActivity.kt
+++ app/src/main/java/com/example/mobile_exercise_w5_ui_login/MainActivity.kt
@@ -6,22 +6,23 @@ import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.layout.width
-import androidx.compose.foundation.layout.wrapContentSize
+import androidx.compose.foundation.layout.wrapContentWidth
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.Divider
 import androidx.compose.material3.OutlinedTextField
-import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
-import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
@@ -31,10 +32,12 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import androidx.compose.ui.text.TextStyle
 import com.example.mobile_exercise_w5_ui_login.ui.theme.Mobile_Exercise_W5_UI_LoginTheme
 
 class MainActivity : ComponentActivity() {
@@ -52,10 +55,10 @@ class MainActivity : ComponentActivity() {
 @Composable
 fun Greeting() {
     Column(
-        modifier = Modifier
-            .fillMaxSize()
-            .wrapContentSize(),
+        modifier = Modifier.fillMaxSize(),
+        // Sắp xếp theo chiều dọc
         verticalArrangement = Arrangement.Center,
+        // Căn chỉnh chiều ngang
         horizontalAlignment = Alignment.CenterHorizontally
     ) {
         Image(
@@ -75,40 +78,136 @@ fun Greeting() {
 
         Spacer(modifier = Modifier.height(20.dp))
 
+        var Email by remember {
+            mutableStateOf("")
+        }
+
+        var password by remember {
+            mutableStateOf("")
+        }
         OutlinedTextField(
-            value = "", onValueChange = {},
-            label = {
-                Text(text = "")
+            value = Email,
+            onValueChange = {
+                Email = it
             },
-            modifier = Modifier.width(width = 280.dp)
-        )
+            textStyle = TextStyle(color = Color.Black),
+            label = {
+                Text(text = "Emaill address", fontSize = 13.sp)
+            },
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(horizontal = 25.dp),
+
+            )
 
         Spacer(modifier = Modifier.height(10.dp))
 
-        OutlinedTextField(value = "", onValueChange = {}, label = {
-            Text(text = "")
-        })
+        OutlinedTextField(
+            value = password,
+            onValueChange = {
+                password = it
+            },
+            label = {
+                Text(text = "Password", fontSize = 13.sp)
+            },
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(horizontal = 25.dp),
+        )
 
         Spacer(modifier = Modifier.height(20.dp))
-
         Button(
             onClick = { /*TODO*/ },
-            modifier = Modifier.width(280.dp),
+            // Shape dung de bo tron cac goc
+            shape = RoundedCornerShape(5.dp),
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(horizontal = 25.dp),
             colors = ButtonDefaults.buttonColors(containerColor = Color(0xFF2196F3)),
         ) {
-            Text(text = "Login in to app")
+            Text(
+                text = "Login in to app",
+                fontFamily = FontFamily.Default,
+                fontWeight = FontWeight.Bold,
+                modifier = Modifier.padding(vertical = 5.dp)
+            )
 
         }
-        var name by remember  {
-            mutableStateOf(0)
-        }
+        Spacer(modifier = Modifier.height(5.dp))
         Button(
-            onClick = { /*TODO*/ },
-            modifier = Modifier.width(280.dp),
-            colors = ButtonDefaults.buttonColors(containerColor = Color.Blue),
-            ) {
-            Text(text = "Reset password")
+            onClick = { },
+            // Shape dung de bo tron cac goc
+            shape = RoundedCornerShape(5.dp),
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(horizontal = 25.dp),
+            colors = ButtonDefaults.buttonColors(containerColor = Color(0xFFE8E9EE)),
+        ) {
+            Text(
+                text = "Forget password ?",
+                color = Color.Black,
+                modifier = Modifier.padding(vertical = 5.dp)
+            )
+        }
 
+        Spacer(modifier = Modifier.height(15.dp))
+        Row(
+            // ********* Hỏi anh Thanh ***** Dòng số 152
+            verticalAlignment = Alignment.CenterVertically,
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(horizontal = 25.dp)
+
+        ) {
+            // Divider dùng để thêm gạch ngang
+            Divider(
+                color = Color.Gray,
+                thickness = 1.dp,
+                modifier = Modifier.weight(1f)
+            )
+            Text(
+                text = "or login with",
+                modifier = Modifier.padding(horizontal = 8.dp),
+                color = Color.Gray,
+            )
+            Divider(
+                color = Color.Gray,
+                thickness = 1.dp,
+                modifier = Modifier.weight(1f)
+            )
+        }
+
+        Row(
+            modifier = Modifier
+                .wrapContentWidth()
+                .padding(vertical = 20.dp),
+            horizontalArrangement = Arrangement.spacedBy(50.dp),
+
+            ) {
+            Image(
+                painter = painterResource(id = R.drawable.twitter_icon),
+                contentDescription = "logo twiter",
+                modifier = Modifier.clickable { },
+            )
+            Image(
+                painter = painterResource(id = R.drawable.google),
+                contentDescription = "logo google",
+                modifier = Modifier.clickable { },
+            )
+            Image(
+                painter = painterResource(id = R.drawable.facebook_icon),
+                contentDescription = "logo facebook",
+                modifier = Modifier.clickable { },
+            )
+        }
+        Row(
+            modifier = Modifier.wrapContentWidth(),
+            horizontalArrangement = Arrangement.spacedBy(60.dp),
+            
+            ) {
+            Text(text = "Tiwter")
+            Text(text = "Google")
+            Text(text = "Facebook")
         }
     }
 }
--- app/src/main/java/com/example/mobile_exercise_w5_ui_login/ui/theme/Type.kt
+++ app/src/main/java/com/example/mobile_exercise_w5_ui_login/ui/theme/Type.kt
@@ -2,9 +2,11 @@ package com.example.mobile_exercise_w5_ui_login.ui.theme
 
 import androidx.compose.material3.Typography
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.sp
+import com.example.mobile_exercise_w5_ui_login.R
 
 // Set of Material typography styles to start with
 val Typography = Typography(
@@ -32,3 +34,4 @@ val Typography = Typography(
     )
     */
 )
+
